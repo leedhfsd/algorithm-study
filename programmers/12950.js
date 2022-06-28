977d7270a49cf1f8d83ad78264bd4727e8503095
leedhfsd <89757700+leedhfsd@users.noreply.github.com>
2022-06-28 22:11:58 +0900

    12950(기본)

---
+++ programmers/12950.js
@@ -0,0 +1,11 @@
+
+function solution(arr1, arr2) {
+    let answer = Array.from({length: arr1.length}, () => []);
+
+    for (let i = 0; i < arr1.length; i++) {
+        for(let j = 0; j < arr1[0].length; j++) {
+            answer[i].push(arr1[i][j] + arr2[i][j]);
+        }
+    }
+    return answer;
+}
