ca74ffaa2d0e97298cfbaf1c58d1fe8c28ca27d4
leedhfsd <89757700+leedhfsd@users.noreply.github.com>
2022-06-28 21:00:57 +0900

    12915(문자열정렬)

---
+++ programmers/12915.js
@@ -0,0 +1,13 @@
+function solution(strings, n) {
+    let answer = strings;
+    answer.sort((a, b) => {
+        if (a[n] > b[n]) return 1;
+        if (a[n] < b[n]) return -1;
+        if (a[n] === b[n]) {
+            if (a > b) return 1;
+            else return -1;
+        }
+    })
+    
+    return answer;
+}
