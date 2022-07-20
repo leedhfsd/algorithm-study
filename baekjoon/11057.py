8ac9c85531762c79cb2ebd5d5efab3a9343f215b
leedhfsd <89757700+leedhfsd@users.noreply.github.com>
2022-07-20 16:40:40 +0900

    11057(DP)

---
+++ baekjoon/11057.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[1] * 10 for _ in range(n+1)] 
+
+for i in range(1, n+1):
+  for j in range(1,10):
+    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 10007
+
+print(dp[n][9])
