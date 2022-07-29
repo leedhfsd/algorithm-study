e1db8b1e1764dfdae53b21545e1e864d6900beb3
leedhfsd <89757700+leedhfsd@users.noreply.github.com>
2022-07-29 14:43:20 +0900

    2003(📌📌투포인터)

---
+++ baekjoon/2003.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+arr = list(map(int,input().split()))
+count = 0
+left = 0
+right = 1
+res = arr[left]
+
+while True:
+  if res >= m:
+    if res == m:
+      count += 1
+    res -= arr[left]
+    left += 1
+  elif right == n:
+    break
+  elif res < m:
+    res += arr[right]
+    right += 1
+
+print(count)
