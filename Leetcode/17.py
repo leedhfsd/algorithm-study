598881f138f7cd7d0d1c250400ced97838b67155
leedhfsd <89757700+leedhfsd@users.noreply.github.com>
2022-10-03 18:39:27 +0900

    17(BFS,DP)

--- Leetcode/17.py
+++ Leetcode/17.py
@@ -18,3 +18,23 @@ class Solution:
               visited.add(tmp)
               dq.append((tmp, cnt+1))
         return -1
+
+from collections import deque
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+        visited = set(coins)
+        dq = deque([])
+        for coin in coins:
+          dq.append((coin, 1))
+        
+        while dq:
+          res, cnt = dq.popleft()
+          if res == amount:
+            return cnt
+          for i in range(len(coins)):
+            tmp = res + coins[i]
+            if tmp not in visited and tmp <= amount:
+              visited.add(tmp)
+              dq.append((tmp, cnt+1))
+        return -1
